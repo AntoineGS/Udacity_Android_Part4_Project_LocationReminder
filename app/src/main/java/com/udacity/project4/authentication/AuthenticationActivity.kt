424ff6d8dfb8812482e9f51d38c2c567c0202aae
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2021-04-30 19:10:51 -0400

    Work on login, activity is not yet called

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,13 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +15,39 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        binding.viewModel = ViewModelProvider(this).get(AuthenticationViewModel::class.java)
+        setContentView(binding.root)
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        binding.viewModel?.userIsAuthenticated?.observe(this, {
+            if (it) {
+                val intent = Intent(this, RemindersActivity::class.java)
+                startActivity(intent)
+            }
+        })
 
+        binding.loginButton.setOnClickListener {
+            requestLogin()
+        }
+    }
+
+    private fun requestLogin() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivity(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.drawable.map)
+                .setIsSmartLockEnabled(false)
+                .setAvailableProviders(providers)
+                .build()
+        )
     }
 }
---
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationViewModel.kt
@@ -0,0 +1,13 @@
+package com.udacity.project4.authentication
+
+import android.app.Application
+import androidx.lifecycle.map
+import com.udacity.project4.base.BaseViewModel
+
+class AuthenticationViewModel(app: Application): BaseViewModel(app) {
+
+    val userIsAuthenticated = FirebaseUserLiveData().map { user ->
+        user != null
+    }
+
+}
