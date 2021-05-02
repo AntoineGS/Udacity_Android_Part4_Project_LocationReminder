78d424392580cb3a55a5e1387a5db11e1a89d396
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2021-05-02 12:03:03 -0400

    Remove file

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -2,25 +2,17 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.IntentSender
-import android.content.pm.PackageManager
-import android.content.res.Resources
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import androidx.core.content.ContextCompat
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
@@ -29,15 +21,23 @@ import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var map: GoogleMap
+    private var isLocationPermissionGranted: Boolean = false
+
+    private val requestLocationPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            isGranted ->
+        isLocationPermissionGranted = isGranted
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_select_location, container, false)
 
@@ -47,45 +47,113 @@ class SelectLocationFragment : BaseFragment() {
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
-//        TODO: add style to the map
-//        TODO: put a marker to location that the user selected
-
-
-//        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        requestLocationPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
 
         return binding.root
     }
 
-    private fun onLocationSelected() {
-        //        TODO: When the user confirms on the selected location,
-        //         send back the selected location details to the view model
-        //         and navigate back to the previous fragment to save the reminder and add the geofence
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.onResume()
+        binding.mapView.getMapAsync(this)
     }
 
+    private fun onLocationSelected() {
+        map.setOnPoiClickListener { poi ->
+            _viewModel.showToast.postValue(getString(R.string.reminder_saved))
+            _viewModel.selectedPOI.postValue(poi)
+            _viewModel.latitude.postValue(poi.latLng.latitude)
+            _viewModel.longitude.postValue(poi.latLng.longitude)
+            _viewModel.reminderSelectedLocationStr.postValue(poi.name)
+            _viewModel.navigationCommand.postValue(NavigationCommand.Back)
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.map_options, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
+    override fun onMapReady(p0: GoogleMap?) {
+        if (p0 != null) {
+            map = p0
+            setPoiClick()
+            onLocationSelected()
+            showMyCurrentLocation()
+        }
+    }
 
+    private fun setPoiClick() {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
+        }
+    }
+
+    @SuppressLint("MissingPermission") // IDE doesn't seem to like when permission check is in another method
+    private fun showMyCurrentLocation() {
+        if (isLocationPermissionGranted) {
+            map.isMyLocationEnabled = true
+            getCurrentLocation()
+
+            val fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(requireContext())
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location.let {
+                        getCurrentLocation()
+
+                        binding.saveButton.setOnClickListener {
+                            location.let {
+                                _viewModel.showToast.postValue(getString(R.string.my_current_location))
+                                _viewModel.latitude.postValue(location?.latitude)
+                                _viewModel.longitude.postValue(location?.longitude)
+                                _viewModel.reminderSelectedLocationStr.postValue(getString(R.string.my_current_location))
+                                _viewModel.navigationCommand.postValue(NavigationCommand.Back)
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    @SuppressLint("MissingPermission") // Doesn't seem to like my bool approach..
+    private fun getCurrentLocation() {
+        if (isLocationPermissionGranted) {
+            fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location ->
+                    location?.let {
+                        val homeLatLng = LatLng(it.latitude, it.longitude)
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, 15F))
+                    }
+                }
+        }
+
+    }
 }
