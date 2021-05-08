4ade3bd0143be8c8eb4fa2d922f10a2c2e24b2f1
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2021-05-08 11:16:27 -0400

    Complete RemindersActivityTest

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,28 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -65,7 +77,67 @@ class RemindersActivityTest :
         }
     }
 
+    @Test
+    fun createReminder_noTitle_errorToEnterTitle() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        activityScenario.use {
+            Espresso.onView(withId(R.id.noDataTextView))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-//    TODO: add End to End testing to the app
+            Espresso.onView(withId(R.id.addReminderFAB)).perform(ViewActions.click())
+            Espresso.onView(withId(R.id.saveReminder)).perform(ViewActions.click())
 
+            Espresso.onView(withId(com.google.android.material.R.id.snackbar_text))
+                .check(ViewAssertions.matches(withText(R.string.err_enter_title)))
+        }
+    }
+
+    @Test
+    fun createReminder_WithoutLocation() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        activityScenario.use {
+            Espresso.onView(withId(R.id.noDataTextView))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+            Espresso.onView(withId(R.id.addReminderFAB)).perform(ViewActions.click())
+
+            Espresso.onView(withId(R.id.reminderTitle))
+                .perform(ViewActions.replaceText("Title"))
+
+            Espresso.onView(withId(R.id.saveReminder)).perform(ViewActions.click())
+
+            Espresso.onView(withId(com.google.android.material.R.id.snackbar_text))
+                .check(ViewAssertions.matches(withText(R.string.err_select_location)))
+        }
+    }
+
+    @Test
+    fun createReminder()  {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        activityScenario.use {
+            Espresso.onView(withId(R.id.noDataTextView))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(
+                    ViewMatchers.Visibility.VISIBLE)))
+
+            Espresso.onView(withId(R.id.addReminderFAB)).perform(ViewActions.click())
+            Espresso.onView(withId(R.id.selectLocation)).perform(ViewActions.click())
+
+            Espresso.onView(withId(R.id.save_button)).perform(ViewActions.click())
+            Espresso.onView(withId(R.id.reminderTitle)).perform(
+                ViewActions.typeText("Title"))
+
+            Espresso.onView(withId(R.id.reminderDescription))
+                .perform(ViewActions.typeText("Description"))
+
+            Espresso.closeSoftKeyboard()
+
+            Espresso.onView(withId(R.id.saveReminder)).perform(ViewActions.click())
+
+            Espresso.onView(withText("Title"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+            Espresso.onView(withText("Description"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        }
+    }
 }
